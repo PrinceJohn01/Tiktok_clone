4771a2fb90ab8eb23d3d4a1527e3a15605dc9631
PrinceJohn01 <princedav748@gmail.com>
2023-09-21 10:55:45 +0100

    Firebase auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,6 +1,40 @@
+import 'dart:io';
 
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/constants.dart';
 
-class AuthController extends  GetxController {
+class AuthController extends GetxController {
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
 
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 
 import 'constants.dart';
+import 'controllers/auth_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value){
-    
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
   });
   runApp(const MyApp());
 }
