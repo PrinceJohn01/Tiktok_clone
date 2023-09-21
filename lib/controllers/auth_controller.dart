094318e6213a4c39c89654e34b3d099929a7a62e
PrinceJohn01 <princedav748@gmail.com>
2023-09-21 12:00:57 +0100

    Firebase auth controller and user

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/models/user.dart' as model;
 
 class AuthController extends GetxController {
   Future<String> _uploadToStorage(File image) async {
@@ -24,11 +26,21 @@ class AuthController extends GetxController {
           email.isNotEmpty &&
           password.isNotEmpty &&
           image != null) {
-        await firebaseAuth.createUserWithEmailAndPassword(
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
         String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+          name: username,
+          email: email,
+          uid: cred.user!.uid,
+          profilePhoto: downloadUrl,
+        );
+        await firestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
       }
     } catch (e) {
       Get.snackbar(
